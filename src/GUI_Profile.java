e45911b9ca16054dfca3872ee319ebc5d1ea422c
jaewon <green5822@nate.com>
2021-04-21 09:44:31 +0900

    방 참가자, 프로필 보기

--- src/GUI_Profile.java
+++ src/GUI_Profile.java
@@ -19,7 +19,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import javax.swing.JList;
 import javax.swing.SwingConstants;
-
+// 수정
 public class GUI_Profile extends JFrame {
 	
 	private DataOutputStream dos;
